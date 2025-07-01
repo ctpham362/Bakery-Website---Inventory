2644f2d3ff1399e041398bb9e3d5f35c2c109164
Meatbuns <phamnothanh362@gmail.com>
2025-06-30 22:17:11 -0700

    New files added: MaximumValidator MinimumValidator ValidMaximum ValidMinimum
    added validators for minimum and maximum inventory values

--- src/main/java/com/example/demo/validators/EnufPartsValidator.java
+++ src/main/java/com/example/demo/validators/EnufPartsValidator.java
@@ -33,7 +33,12 @@ public class EnufPartsValidator implements ConstraintValidator<ValidEnufParts, P
         if (product.getId() != 0) {
             Product myProduct = repo.findById((int) product.getId());
             for (Part p : myProduct.getParts()) {
-                if (p.getInv()<(product.getInv()-myProduct.getInv()))return false;
+                if (p.getInv()<(product.getInv()-myProduct.getInv()))
+                {
+                    constraintValidatorContext.disableDefaultConstraintViolation();
+                    constraintValidatorContext.buildConstraintViolationWithTemplate("Not enough inventory for: " + p.getName()).addConstraintViolation();
+                    return false;
+                }
             }
             return true;
         }
